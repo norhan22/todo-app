37b237c28c521036a5ecbb682153c3b7c2b93cda
norhan <nora.moh.2011@gmail.com>
2022-07-23 01:32:39 +0200

    feat : add electron hot reloader && fix node sass version

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { app } from 'electron'
+import { app, BrowserWindow } from 'electron'
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -6,6 +6,11 @@ app.on('window-all-closed', function () {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
+// Standard stuff
 
 // Load here all startup windows
 require('./mainWindow')
+
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
