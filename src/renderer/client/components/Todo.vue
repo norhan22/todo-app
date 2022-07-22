2951835c64c60f82d341150031ed420fd99b1663
norhan <nora.moh.2011@gmail.com>
2022-07-23 01:35:41 +0200

    feat : create simple TODO App

---
+++ src/renderer/client/components/Todo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="e-nuxt-container">
+    <div class="e-nuxt-content">
+      <div class="e-nuxt-logo">
+        <h1>TODO</h1>
+        <form class="e-nuxt-form" @submit.prevent="save">
+          <label>Add new Task</label>
+          <input v-model="newTask" type="text">
+        </form>
+        <ul>
+          <li v-for="(task,i) in list" :key="i">
+            <input v-model="task.isDone" type="checkbox">
+            <span :style="task.isDone?'text-decoration: line-through;':''">{{ task.content }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="e-nuxt-system-info" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data: () => {
+    return {
+      newTask: '',
+      list: []
+    }
+  },
+  methods: {
+    save () {
+      if (!this.newTask) return false
+      else {
+        this.list.push({
+          content: this.newTask,
+          isDone: false
+        })
+        this.newTask = ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/client/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <TODO />
+</template>
+
+<script>
+import TODO from '@/components/Todo.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    TODO
+  },
+  data () {
+    return {
+      externalContent: ''
+    }
+  },
+  methods: {
+    openURL (url) {
+      window.open(url)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
