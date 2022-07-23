4ce2cab79825e6f2fa5d7e987b985bc372481d45
norhan <nora.moh.2011@gmail.com>
2022-07-23 03:11:55 +0200

    feat : setup server side to save TODO tasks Offline with electron-data-holder

--- src/main/index.js
+++ src/main/index.js
@@ -1,16 +1,26 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from "electron";
 
+const { initDB } = require("electron-data-holder");
+
+// the encryption key must be 32 characters long.
+
+initDB({
+  // key: 'the-encryption-key',
+  customPath: "src/DB/",
+});
+
+// pass null instead of the key if you don't wan't to pass it but you want to pass a folder path;
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
 // Standard stuff
 
 // Load here all startup windows
-require('./mainWindow')
+require("./mainWindow");
 
 try {
-  require('electron-reloader')(module)
+  require("electron-reloader")(module);
 } catch (_) {}
