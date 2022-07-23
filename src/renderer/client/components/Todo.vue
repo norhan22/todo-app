40e1db83d47df2a4074c950f244598924e31864c
norhan <nora.moh.2011@gmail.com>
2022-07-23 04:28:25 +0200

    feat : finish CRUD System Integration

--- src/renderer/client/components/Todo.vue
+++ src/renderer/client/components/Todo.vue
@@ -2,20 +2,35 @@
   <div class="e-nuxt-container">
     <div class="e-nuxt-content">
       <div class="e-nuxt-logo">
+        <p class="msg-container">
+          <span class="error">{{ error }}</span>
+          <span class="success">{{ successMsg }}</span>
+        </p>
         <h1>TODO</h1>
-        <form class="e-nuxt-form" @submit.prevent="save">
+        <form class="e-nuxt-form" @submit.prevent="addNew">
           <label>Add new Task</label>
           <input v-model="newTask" type="text">
-          <span v-if="error" class="error">{{ error }}</span>
+
         </form>
         <ul>
-          <li v-for="(task,i) in tasks" :key="i">
-            <input v-model="task.isDone" type="checkbox">
-            <span :style="task.isDone?'text-decoration: line-through;':''">{{ task.content }}</span>
+          <li v-for="(task,i) in tasks" :key="i" class="task">
+            <div v-if="!task.editable">
+              <input v-model="task.isDone" type="checkbox" @change="taskDone(task)">
+              <span :class="{'done':task.isDone}"> {{ task.content }}  </span>
+            </div>
+            <form class="e-nuxt-form" @submit.prevent="edit(task)" v-else>
+              <input type="text" v-model.trim="task.content">
+            </form>
+            <div>
+              <button type="button" v-if="!task.editable" @click="()=>{task.editable = true}">Edit</button>
+              <button type="button" @click="deleteTask(task.id)" class="danger">X</button>
+            </div>
+
+
           </li>
         </ul>
       </div>
-      <div class="e-nuxt-system-info"/>
+
     </div>
   </div>
 </template>
@@ -23,19 +38,28 @@
 <script>
 import server from '../../server/index.js'
 
+
 export default {
   name: 'Todo',
   data: () => {
     return {
       newTask: '',
       tasks: [],
-      error: ''
+      error: '',
+      successMsg: ''
 
     }
   },
-  computed: {},
+  computed: {
+    todoRepo() {
+      return server.todo
+    }
+  },
   methods: {
-    save() {
+    ///////////////////
+    // APIs
+    //////////////////
+    addNew() {
       if (!this.newTask) return false
       else {
         server.todo.addTask({
@@ -46,6 +70,7 @@ export default {
               console.log(res)
               this.tasks = res
               this.newTask = ''
+              this.successMsg = 'Added Successfully'
             })
             .catch((err) => {
               this.error = err
@@ -53,10 +78,53 @@ export default {
 
 
       }
+    },
+    taskDone(task) {
+      this.todoRepo.doneTask(task.id, task.isDone)
+          .then((res) => {
+            this.successMsg = 'Saved Successfully'
+          })
+          .catch((err) => {
+            this.error = err
+            task.isDone = !task.isDone
+          })
+
+    },
+    deleteTask(id) {
+
+      this.todoRepo.removeTask(id)
+          .then(res => {
+            this.tasks = res
+            this.successMsg = 'Deleted Successfully'
+          })
+          .catch((err) => {
+            this.error = err
+
+          })
+    },
+    edit(task) {
+      this.todoRepo.editTask(task.id, task)
+          .then((res) => {
+            task.editable = false
+            this.successMsg = 'Saved Successfully'
+          })
+          .catch((err) => {
+            this.error = err
+
+          })
+
     }
+
   },
   created() {
-    this.tasks = server.todo.getAllTasks()
+    this.tasks = this.todoRepo.getAllTasks()
+  },
+  updated() {
+    if (this.error || this.successMsg) {
+      setTimeout(() => {
+        this.error = this.successMsg = ''
+      }, 5000)
+    }
   }
 }
 </script>
