e9f54fcdce3dc2808d45a87933f8266fbe904c24
norhan <nora.moh.2011@gmail.com>
2022-07-25 15:25:00 +0200

    separate init DB code and import it in mainWindow.js

---
+++ src/main/initBDFolder.js
@@ -0,0 +1,23 @@
+import { app } from "electron";
+
+const { initDB } = require("electron-data-holder"),
+  dataPath = app.getPath("appData"),
+  fs = require("fs"),
+  DBFolderPath = `${dataPath}/todo`;
+
+// Create new folder for the app store
+try {
+  if (!fs.existsSync(DBFolderPath)) {
+    fs.mkdirSync(DBFolderPath);
+  }
+} catch (err) {
+  console.error(err);
+}
+
+// Init DB
+initDB({
+  // key: 'the-encryption-key',
+  customPath: DBFolderPath,
+});
+
+export default DBFolderPath;
